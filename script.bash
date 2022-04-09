e1eddfdbbaa66774f953d0836ebfcd292fec9726
Grandes <ho2708@naver.com>
2022-04-09 20:03:00 +0900

    feat(class code clone): In class
    In class bash script 정리

--- script.bash
+++ script.bash
@@ -1,15 +1,11 @@
-
-
-
 # network db 생성
 docker network create db
-
 # network db 삭제
 docker network rm db
 
-# kwon-yongho directory > db-data 에서
+# kwon-yongho directory
+##### db-data directory에서
 docker run --name db -p 5432:5432 --network=db -v "$PWD:/var/lib/postgresql/data" -e POSTGRES_PASSWORD=password -d postgres:alpine
-
 # docker db 끄기
 docker rm -f db
 
